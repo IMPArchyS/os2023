16244ddd9343a62048013466bf6394bafd51c667
Daniel Janik <01djanik@gmail.com>
2023-11-02 10:55:44 +0100

    feat: uthread

--- user/uthread.c
+++ user/uthread.c
@@ -10,14 +10,33 @@
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
 
+struct context {
+  uint64 ra;
+  uint64 sp;
+
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+
+  struct context con;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
-extern void thread_switch(uint64, uint64);
+extern void thread_switch(struct context*,struct context*);
               
 void 
 thread_init(void)
@@ -60,6 +79,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+    thread_switch(&t->con, &next_thread->con);
   } else
     next_thread = 0;
 }
@@ -74,6 +94,8 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  t->con.sp = (uint64) t->stack + STACK_SIZE;
+  t->con.ra = (uint64) func;
 }
 
 void 
