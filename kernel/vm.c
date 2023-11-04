11551bbda297bf71a33c2f330e519259e7c41e22
Daniel Janik <01djanik@gmail.com>
2023-11-04 17:08:23 +0100

    feat: vmprint

--- kernel/vm.c
+++ kernel/vm.c
@@ -449,3 +449,26 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void vmprint(pagetable_t pt) 
+{
+  static int level = 0;
+  if (level == 0)
+    printf("page table %p\n", pt);
+
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pt[i];
+    if (pte & PTE_V) {
+      for (int j = 0; j <= level; j++)
+        printf(" ..");
+
+      printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+
+      if ((pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+        level++;
+        vmprint((pagetable_t)(PTE2PA(pte)));
+        level--;
+      }
+    }
+  }
+}
